190440bf6243d2f8276f1727ef219e3e393fb6a4
ntijoh-mathias-malmqvist <143790852+ntijoh-mathias-malmqvist@users.noreply.github.com>
2023-11-10 15:12:53 +0100

    started ForkListVeiw

--- frontend/src/views/ForkListView.vue
+++ frontend/src/views/ForkListView.vue
@@ -1,4 +1,37 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+
+const forksURL = "https://api.github.com/repos/ntijoh-lukas-johansson/for-p2-1/forks";
+
+const manifest = `{
+    "assignmentName": "Smallest of Two",
+    "filePath": "lib/smallest_of_two.js",
+    "language": "javascript",
+    "functionName": "smallestOfTwo",
+    "functionArity": 2,
+    "tests": [
+        {"description": "First is smallest",
+        "arguments": [1,2],
+        "expected": 1},
+        {"description": "Second is smallest",
+        "arguments": [2,1],
+        "expected": 1},
+        {"description": "Same size",
+        "arguments": [2,2],
+        "expected": 2}
+    ]
+    }`
+
+const options = "key";
+
+  async function loadForks() {
+    const forksResponse = await fetch(forksURL, options);
+    const forks = await forksResponse.json();
+
+  }
+
+</script>
+
+
 
 <template></template>
 
